bae554b11d9b4580e63e872ebe02e59f228efa3f
Mobin Ghoveoud <mobin.sgh@gmail.com>
2023-08-25 10:17:23 +0330

    feat: Initial commit

---
+++ src/core/settings/local.py
@@ -0,0 +1,10 @@
+from .base import *  # noqa: F403
+
+DEBUG = True
+
+# Django debug toolbar
+if DEBUG:
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
